cf55070933d511394c89c5e65683f7429dad722f
zerry101 <macone394@gmail.com>
2022-09-18 14:35:21 +0530

    m

--- src/app/componets/login/login.component.ts
+++ src/app/componets/login/login.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   type:string="password";
-  eyeIcon:string='fill';
+  eyeIcon:string='slash-fill';
   isText:boolean=false;
 
   constructor() { }
